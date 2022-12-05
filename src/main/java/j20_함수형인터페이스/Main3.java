bc0806283d20da89aa4b9f967777edb6f5ae616e
chraven <kcrow999@naver.com>
2022-12-05 12:57:47 +0900

    챕터 20 함수형 인터페이스 마무리

--- src/main/java/j20_함수형인터페이스/Main3.java
+++ src/main/java/j20_함수형인터페이스/Main3.java
@@ -1,7 +1,11 @@
 package j20_함수형인터페이스;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class Main3 {
@@ -23,8 +27,9 @@ public class Main3 {
 		names.add("김사희");
 		
 		names.forEach(n -> {//forEach(메소드)가 Consumer라는 인터페이스 를 매개변수로 받는다. 여기선 names라는 리스트로 연결됨.
-			//인터페이스 객체를 업캐스팅되어 매개변수로 들어감. accept의 매개변수가 n <? super T>로 T의부모까지 매개변수로. 
+			//업캐스팅되어 매개변수로 들어갈 수 있음. accept의 매개변수가 n <? super T>로 T의부모까지 매개변수로. 
 			//names에서 <String>이므로 T는 String. action이 람다식으로 만든 객체.
+			// n: Consumer 객체를 생성할 때 들어간 자료형으로 받는다.(String이 들어갔으므로 String)
 			System.out.println(n);//accept를 오버라이드.
 		});
 		
@@ -34,5 +39,34 @@ public class Main3 {
 		//consumer에는 람다식을 담기 위한 것.
 		//n -> {System.out.println(n);}는 위의 Consumer에 연결되어 그 안의 유일한 메소드인 accept를 오버라이드 한 것
 		//Consumer는 반환값이 없기 때문에 실행문을 그대로 실행(출력)하고 끝남.
+		
+		System.out.println();
+		
+		Map<String, String> subject = new HashMap<>();
+		
+		subject.put("java", "자바");
+		subject.put("python", "파이썬");
+		subject.put("javascript", "자바스크립트");
+		subject.put("C", "씨언어");
+		subject.put("Node.js", "노드제이에스");
+		
+		Set<String> keySet = subject.keySet();//keySet()은 Set형태로 key를 가져온다.
+		keySet.forEach(key -> {
+			
+			System.out.println(subject.get(key));
+		});
+		
+		System.out.println();
+		
+		Set<Entry<String, String>> entrySet = subject.entrySet();
+		entrySet.forEach(entry -> {
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		});
+		
+		System.out.println();
+		
+		subject.forEach((key, value) -> {
+			System.out.println(key + ": " + value);
+		});
 	}
 }
---
+++ src/main/java/j20_함수형인터페이스/Main4.java
@@ -0,0 +1,39 @@
+package j20_함수형인터페이스;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class Main4 {
+	
+	public static void main(String[] args) {
+		
+		Function<Integer, String> fx1 = age -> age + "살";
+		//앞이 매개변수 뒤가 리턴타입
+		
+		System.out.println(fx1.apply(20));
+		
+		Function<Function<Integer, String>, String> fx2 = fx -> fx.apply(20) + "입니다";
+		
+		System.out.println(fx2.apply(age -> age + "살"));//람다 안에 람다를 쓴 것. 리턴 자체를 function으로 주는 것도 가능.
+		
+		BiFunction<Integer, String, Map<Integer, String>> createMap = (number, name) -> {
+			//매개변수 두개, 리턴 Map. 두 매개변수로 map을 만들어 리턴한다.
+			Map<Integer, String> map = new HashMap<>();
+			map.put(number, name);
+			return map;
+		};
+		
+		List<Map<Integer, String>> list = new ArrayList<>();
+		list.add(createMap.apply(100,  "김영희"));
+		list.add(createMap.apply(200,  "김일희"));
+		list.add(createMap.apply(300,  "김이희"));
+		list.add(createMap.apply(400,  "김삼희"));
+		list.add(createMap.apply(500,  "김사희"));
+		
+		System.out.println(list);
+	}
+}
